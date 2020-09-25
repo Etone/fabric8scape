0d9bf36f5eda1861c0521a2a5943b94b67161a9e
Corvin Schapöhler <corvin.schapoehler@novatec-gmbh.de>
2020-09-25 10:53:49 +0200

    Make all Applications run-able
    Set all needed Configflags so the applications can start without any problem

--- registry/src/test/java/de/novatec/fabric8scape/registry/RegistryApplicationTests.java
+++ registry/src/test/java/de/novatec/fabric8scape/registry/RegistryApplicationTests.java
@@ -4,10 +4,11 @@ import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
-class RegsitryApplicationTests {
+class RegistryApplicationTests {
 
 	@Test
 	void contextLoads() {
+		assert(true);
 	}
 
 }
