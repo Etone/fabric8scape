a6139b9245bdd09f8f2e25f27562ff4859406d15
Corvin Schapöhler <corvin.schapoehler@novatec-gmbh.de>
2020-10-27 12:08:24 +0100

    Add create form

--- admin-ui/src/views/Admin.vue
+++ admin-ui/src/views/Admin.vue
@@ -2,7 +2,31 @@
   <div class="home">
     <h1 class="text-h1 header">Admin</h1>
     <v-divider />
-
+    <h2 class="text-h2 header">Create New Pool</h2>
+    <v-forms @submit.prevent="this.createPool()">
+      <v-row>
+        <v-col cols="4">
+          <v-text-field v-model="newPool.image.repository" label="Repository" />
+        </v-col>
+        <v-col cols="1">
+          <v-text-field v-model="newPool.image.tag" label="Tag" />
+        </v-col>
+        <v-col cols="2">
+          <v-text-field v-model="newPool.creator.system" label="System" />
+        </v-col>
+        <v-col cols="2">
+          <v-text-field
+            v-model="newPool.creator.environment"
+            label="Environment"
+          />
+        </v-col>
+        <v-col cols="2">
+          <v-text-field v-model="newPool.creator.version" label="Version" />
+        </v-col>
+        <v-col cols="1"><v-btn color="green" fab @click="createPool()"><v-icon color="white">fas fa-plus-circle</v-icon></v-btn></v-col>
+      </v-row>
+    </v-forms>
+    <v-divider />
     <v-data-table :headers="headers" :items="items" class="elevation-1">
       <template v-slot:item.controls="{ item }">
         <v-btn fab dark small color="grey" @click="deployPool(item.id)">
@@ -42,6 +66,10 @@ export default {
           sortable: false,
         },
       ],
+      newPool: {
+        image: {},
+        creator: {},
+      },
     };
   },
   asyncComputed: {
@@ -74,8 +102,9 @@ export default {
     deletePool: async function (id) {
       await this.$adminService.deleteDataPool(id);
     },
-    createPool: async function(pool) {
-      await this.$adminService.createDataPool(pool);
+    createPool: async function () {
+      console.log(this.newPool);
+      await this.$adminService.createDataPool(this.newPool);
     },
   },
 };
